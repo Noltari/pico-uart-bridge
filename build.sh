beb34da36d1717aafe212b6bef4b8e8ef2c3d599
Álvaro Fernández Rojas <noltari@gmail.com>
2021-02-06 11:14:31 +0100

    build: automatically checkout pico-sdk
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 BUILD_DIR=build
+PICO_SDK_DIR=pico-sdk
 
 main () {
 	local cur_dir=$PWD
 
+	if [ ! -d "$PICO_SDK_DIR/.git" ]; then
+		git submodule update --init --recursive
+	fi
+
 	mkdir -p $BUILD_DIR
 	cd $BUILD_DIR
-	cmake ..
+	cmake ../
 	make
+
 	cd $cur_dir
 }
 
